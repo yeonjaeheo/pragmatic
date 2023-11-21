706eb16087cffaa9eeb8e3f6b303bac47f15b6b2
yeonjaeheo <heoyeonjae@heoyeonjaeui-MacBookAir.local>
2023-11-21 16:58:32 +0900

    first commit

---
+++ accountapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from accountapp.views import hello_world
+
+app_name = "accountapp"
+
+# "accountapp:hello_world"
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world')
+]
