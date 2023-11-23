863d6d61726fe780475cbcc69147d039f522b29d
yeonjaeheo <heoyeonjae@heoyeonjaeui-MacBookAir.local>
2023-11-23 16:58:30 +0900

    django course 14 commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# 동적인 것을 db와 연동시켜 원하는대로 바꿀 수 있도록 만드는 작업을 models.py 에서 한다.
+
+class HelloWorld(models.Model): 
+    text = models.CharField(max_length=255, null=False)
+    
