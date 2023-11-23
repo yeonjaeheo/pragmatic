37cb2a84c9f125f1d03913f500ab4013ae774774
yeonjaeheo <heoyeonjae@heoyeonjaeui-MacBookAir.local>
2023-11-23 13:18:34 +0900

    Django course 8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def hello_world(request):
-    return HttpResponse('hello world!')
+    return render(request, 'base.html')
