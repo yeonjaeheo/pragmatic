0507f26115cf689622734ed7857a94ff5c772bd6
yeonjaeheo <heoyeonjae@heoyeonjaeui-MacBookAir.local>
2023-11-24 16:32:16 +0900

    django course 17 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!'})
+        ## post 방식으로 보낸 입력값을 temp 변수를 만들어서 return 해낸다.
+        temp = request.POST.get('hello_world_input')
+        ## 입력값을 model 에 저장한다.
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
