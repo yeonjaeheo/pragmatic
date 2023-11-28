1433bf7f633b99478f7007f5f0ca7ce8f95d6b94
yeonjaeheo <yeonjaeheo@naver.com>
2023-11-28 19:16:18 +0900

    django  course 29 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
