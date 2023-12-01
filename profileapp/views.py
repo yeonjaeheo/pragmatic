9d00cf1a806daa00106dc93113b2dfaf766696e2
yeonjaeheo <yeonjaeheo@naver.com>
2023-12-01 19:33:00 +0900

    django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -11,15 +11,18 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
     
-  
+    
     def form_valid(self, form):
         temp_profile = form.save(commit=False)
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+    
+    
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk' : self.object.user.pk})
     
     
 @method_decorator(profile_ownership_required, 'get')
@@ -28,5 +31,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+    
+    
+    def get_success_url(self):  
+        return reverse_lazy('accountapp:detail', kwargs={'pk' : self.object.user.pk})
