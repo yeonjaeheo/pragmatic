90356414356764afc100f314960544bf8d1b462e
yeonjaeheo <yeonjaeheo@naver.com>
2023-12-04 21:57:49 +0900

    django course 41 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,8 +9,10 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.models import HelloWorld
+from articleapp.models import Article
 
 
 has_ownership = [account_ownership_required, login_required]
@@ -45,10 +47,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
     
     
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    paginate_by = 25
+    
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        
     
     
 @method_decorator(has_ownership, 'get')
--- articleapp/models.py
+++ articleapp/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from projectapp.models import Project
+
 # Create your models here.
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    # 아래는 projectapp 만든 후에 추가한 것
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+    
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
